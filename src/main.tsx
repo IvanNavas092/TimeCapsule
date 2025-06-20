cdf1933b487ddb702127a015a5b9d8ce8f2fbd1e
Jorge <jorgevivar2005@gmail.com>
2025-06-20 20:56:45 +0200

    Implement session restoration and improve Auth0 integration in App component
    - Added useEffect hook to restore user session silently if not authenticated.
    - Updated Auth0Provider in main.tsx to use consistent naming for domain and client ID.
    - Enhanced authentication parameters with scope and local storage caching.

--- src/main.tsx
+++ src/main.tsx
@@ -5,17 +5,20 @@ import App from './App.tsx';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { AUTH0_CLIENT_ID, AUTH0_DOMAIN } from './utils/constants.ts';
 
-const domain = AUTH0_DOMAIN;
-const clientId = AUTH0_CLIENT_ID;
+const DOMAIN = AUTH0_DOMAIN;
+const CLIENTID = AUTH0_CLIENT_ID;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Auth0Provider
-      domain={domain}
-      clientId={clientId}
+      domain={DOMAIN}
+      clientId={CLIENTID}
       authorizationParams={{
         redirect_uri: window.location.origin,
-      }}>
+        scope: 'openid profile email',
+      }}
+      cacheLocation="localstorage"
+      useRefreshTokens={true}>
       <App />
     </Auth0Provider>
   </StrictMode>
