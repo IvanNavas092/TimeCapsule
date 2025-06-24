092499e55f2461e2a93e3ec188183f950ce3dc1e
IvanNavas092 <navass21112@gmail.com>
2025-06-24 14:41:21 +0200

    change of hero!

--- src/components/sections/subhero.tsx
+++ src/components/sections/subhero.tsx
@@ -3,10 +3,8 @@
 export function Subhero() {
   return (
     <>
-      <div className="bg-[var(--color-primary)] h-screen">
-        <button className=" text-white text-2xl font-bold p-4 bg-blue-950/20 rounded-md border-1 border-blue-500 shadow-md shadow-blue-300">
-          Pulsame!
-        </button>
+      <div className="z-10 relative h-screen">
+       
       </div>
     </>
   );
